3bef061b086d20f04aba670836693b3da7835f4f
Muhammad Ur Rehman <rehman@weguide.health>
2025-12-09 10:15:18 +0500

    Initial commit

---
+++ tilesets/weguide35.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="weguide logo 35x35" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../../../weguide logo 35x35.png" trans="ff00ff" width="35" height="35"/>
+</tileset>
